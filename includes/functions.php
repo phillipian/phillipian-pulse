9c27d45cca437925df44335263eed9d6e6595f22
ericouyang <contact@ericouyang.com>
2012-08-27 19:06:09 -0300

    more regex tweaking

--- includes/functions.php
+++ includes/functions.php
@@ -3,7 +3,7 @@
 // email regex based on http://www.regular-expressions.info/email.html
 
 function linkify($text){
-  $text = preg_replace("#(^|[\n ])([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})#", "<a href='mailto:$1'>$1</a>", $text);
+  $text = preg_replace("#([A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4})#", "<a href='mailto:$1'>$1</a>", $text);
   $text = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $text);
   $text = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $text);
   $text = preg_replace("/[ ]@(\w+)/", " <a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $text);
