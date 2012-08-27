2d856715bed1a7e773fb0d54d3a20225e606b953
ericouyang <contact@ericouyang.com>
2012-08-27 19:14:36 -0300

    another regex

--- includes/functions.php
+++ includes/functions.php
@@ -1,9 +1,9 @@
 <?php
 // http://www.achari.in/php-regex-to-make-twitter-links-clickable - Based on
-// email regex based on http://www.regular-expressions.info/email.html
+// email regex from http://stackoverflow.com/questions/3982483/php-regular-expression-text-url-to-html-link
 
 function linkify($text){
-  $text = preg_replace("/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/", "<a href='mailto:$1'>$1</a>", $text);
+  $text = preg_replace("/([\w\-\d]+\@[\w\-\d]+\.[\w\-\d]+)/", "<a href='mailto:$1'>$1</a>", $text);
   $text = preg_replace("#(^|[\n ])([\w]+?://[\w]+[^ \"\n\r\t< ]*)#", "\\1<a href=\"\\2\" target=\"_blank\">\\2</a>", $text);
   $text = preg_replace("#(^|[\n ])((www|ftp)\.[^ \"\t\n\r< ]*)#", "\\1<a href=\"http://\\2\" target=\"_blank\">\\2</a>", $text);
   $text = preg_replace("/[ ]@(\w+)/", " <a href=\"http://www.twitter.com/\\1\" target=\"_blank\">@\\1</a>", $text);
